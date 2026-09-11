6e5f9edbd692e1e7851e635ca0244c8b1af3bbf5
sanjayk6367 <184069761+sanjayk6367@users.noreply.github.com>
2026-09-11 12:42:43 +0530

    this is first comment for my python journey

---
+++ day1.py
@@ -0,0 +1,16 @@
+#%%
+print("hello world")
+
+#%%
+name = input("Enter your name: ")
+print("Hello, ",name)
+
+#%%
+name = "sanjay"
+age = 22
+rollNumber = 148
+
+print(name,age,rollNumber)
+
+#%%
+
