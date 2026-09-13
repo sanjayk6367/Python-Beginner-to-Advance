7f85ce57ed66dd3910495727cd810258048f06af
sanjayk6367 <184069761+sanjayk6367@users.noreply.github.com>
2026-09-13 18:31:54 +0530

    day2 pactice 'variables'

--- day2.py
+++ day2.py
@@ -1,3 +1,24 @@
 #%%
 
 # Variables
+
+name = "sanjay"
+age = 22
+student_name = "sanjay"
+age1 = 22
+Name = "sanjay"
+_email = "sanjay1234@gmail.com"
+email = "sanjay1234@gmail.com"
+
+print(name,age,student_name,age1,Name,_email,email)
+
+## this formate is allowed in pythonn for variables above variables are only for practice purpose/examples 
+
+
+# %%
+# implementation practice of variables
+name = "Sanjay Kumawat"
+age = 22
+city = "Jaipur"
+
+print("My name is - ",name, "and my age is - ",age, "and i live in - ", city)
