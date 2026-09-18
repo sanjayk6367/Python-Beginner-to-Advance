630486f2746abe4313219087b525d7cac170a4c2
sanjayk6367 <184069761+sanjayk6367@users.noreply.github.com>
2026-09-18 23:23:17 +0530

    boolean, list , string

--- day3.py
+++ day3.py
@@ -19,3 +19,51 @@ print(z)
 print(type(z))
 
 # %%
+# string (str)
+
+name = "Sanjay Kumawat"
+college = "Arya College of Engineering & IT"
+
+
+
+# %%
+print(name,college)
+
+
+# %%
+
+#boolean (bool)
+
+is_student = True
+is_teacher = False 
+
+print(type(is_student))
+print(type(is_teacher))
+
+print(is_student)
+print(is_teacher)
+
+age = 22
+print(age >= 18)
+
+age =33
+print(age >=45)
+
+
+# %%
+
+# list
+
+marks = [85, 90, 78, 92, 88]
+print(marks)
+print(type(marks))
+
+student = ["Sanjay", "Arya College", 22, True]
+print(student,type(student))
+
+
+# %%
+
+marks = [85, 90, 78, 92, 88]
+print(marks[0])  # Accessing the first element
+print(marks[2])  # Accessing the third element  
